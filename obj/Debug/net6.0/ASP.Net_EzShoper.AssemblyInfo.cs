48c14442eb4d3da1bbc21652f44c0bc5d706022a
Pham Nhu Son <phamnhusondolla@gmail.com>
2024-10-16 23:40:48 +0700

    fix banner User become Momo

--- obj/Debug/net6.0/ASP.Net_EzShoper.AssemblyInfo.cs
+++ obj/Debug/net6.0/ASP.Net_EzShoper.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ASP.Net_EzShoper")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32dd7f9e8560157710fbe9d04f823465c63a16b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb4c47acf9d53afd61fcbb909b0454636c3039da")]
 [assembly: System.Reflection.AssemblyProductAttribute("ASP.Net_EzShoper")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ASP.Net_EzShoper")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
