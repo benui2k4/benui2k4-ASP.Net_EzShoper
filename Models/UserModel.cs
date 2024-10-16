01247812e29d8758648dbd0517cce1ea21943c22
Pham Nhu Son <phamnhusondolla@gmail.com>
2024-10-16 10:00:54 +0700

    fix model UserModel register validation in User

--- Models/UserModel.cs
+++ Models/UserModel.cs
@@ -12,10 +12,13 @@ namespace ASP.Net_EzShoper.Models
         [Required(ErrorMessage = "Email không được để trống!"), EmailAddress(ErrorMessage = "Vui lòng nhập đúng định dạng email!")]
         public string Email { get; set; }
 
-        [Required(ErrorMessage = "PhoneNumber không được để trống!")]
+        [Required(ErrorMessage = "PhoneNumber không được để trống !")]
+        [RegularExpression(@"^\d{10}$", ErrorMessage = "Vui lòng nhập đúng định dạng Phone !")]
         public string PhoneNumber { get; set; }
 
-		[DataType(DataType.Password),Required(ErrorMessage ="Password không được để trống!")]
-		public string Password { get; set; }
-	}
+        [Required(ErrorMessage = "Password không được để trống!")]
+        [DataType(DataType.Password)]
+        [RegularExpression(@"^(?=.*[a-zA-Z])(?=.*\d)(?=.*[\W_]).+$", ErrorMessage = "Mật khẩu phải chứa ít nhất một chữ cái, một số và một ký tự đặc biệt.")]
+        public string Password { get; set; }
+    }
 }
